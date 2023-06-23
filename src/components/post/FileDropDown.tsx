0e21f701c193ea64dc333fa55a94ee49137adcb5
dlrjs2360 <lhg3670@naver.com>
2023-06-23 11:22:03 +0900

    feat : 메인페이지 게시글 카드 틀 개발

---
+++ src/components/post/FileDropDown.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+
+const MyDropdown = () => {
+  const [selectedValue, setSelectedValue] = useState('');
+
+  const handleChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+    setSelectedValue(event.target.value);
+  };
+
+  return (
+    <div>
+      <Select onChange={handleChange}>
+        <MenuItem value="option1">Option 1</MenuItem>
+        <MenuItem value="option2">Option 2</MenuItem>
+        <MenuItem value="option3">Option 3</MenuItem>
+      </Select>
+    </div>
+  );
+};
+
+export default MyDropdown;
