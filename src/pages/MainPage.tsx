6cbd0faf253ab862fbbb9e49d01df48851655738
mon0mon <mon0mon@naver.com>
2023-06-27 23:38:13 +0900

    fix: 코드 수정

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,12 +1,15 @@
 import CpButton from '../components/atoms/CpButton';
-import RecommendPost from "../components/organisms/RecommendPost";
+import RecommendPost from '../components/organisms/RecommendPost';
 // @ts-ignore
-import {FormControl, InputLabel, MenuItem, SelectChangeEvent} from "@mui/material";
-import DropDown from "../components/molecules/DropDown";
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  SelectChangeEvent,
+} from '@mui/material';
+import DropDown from '../components/molecules/DropDown';
 
 export default () => {
-
-
   return (
     <>
       <div>main page</div>
