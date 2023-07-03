1a1df6b7cbec02e3a5ec20dd6480230744d94200
mon0mon <mon0mon@naver.com>
2023-07-03 18:17:51 +0900

    Refactor : MainPage - MainPageFrame으로 컴포넌트 분리
    Feat : MainPageFrame
    - DropDown 컴포넌트를 이용해 기업태그, 정렬 추가
    
    Feat : Dropdown
    - DropDown 컴포넌트 추가

--- src/components/molecules/DropDown.tsx
+++ src/components/molecules/DropDown.tsx
@@ -2,30 +2,45 @@ import {FormControl, InputLabel, MenuItem, Select, SelectChangeEvent} from "@mui
 import React from "react";
 
 type DropDownProps = {
+  title: string,
   items: string[],
-  values: string[];
+  values: string[],
+  defaultValue: string;
 }
 
-function Dropdown() {
-  const [age, setAge] = React.useState('');
+function Dropdown(props: DropDownProps) {
+  const [choice, setChoice] = React.useState(props.defaultValue);
 
   const handleChange = (event: SelectChangeEvent) => {
-    setAge(event.target.value as string);
+    setChoice(event.target.value as string);
   };
+
+  if (props.items.length != props.values.length) {
+    console.error(`Dropdown ${props.title} has error
+    props.items.length and props.values.length are not equal`);
+    return <span>Dropdown {props.title} error</span>
+  }
+
+  const dropdownItems: any[] = [];
+  for (let i = 0; i < props.items.length; i++) {
+    dropdownItems.push(
+      <MenuItem value={props.values[i]} key={i}>
+        {props.items[i]}
+    </MenuItem>);
+  }
 
   return (
     <FormControl fullWidth>
-      <InputLabel id="demo-simple-select-label">Age</InputLabel>
+      <InputLabel id="demo-simple-select-label">{props.title}</InputLabel>
       <Select
         labelId="demo-simple-select-label"
         id="demo-simple-select"
-        value={age}
-        label="Age"
+        value={choice}
+        label={props.title}
         onChange={handleChange}
+        defaultValue={props.defaultValue}
       >
-        <MenuItem value={10}>Ten</MenuItem>
-        <MenuItem value={20}>Twenty</MenuItem>
-        <MenuItem value={30}>Thirty</MenuItem>
+        {dropdownItems}
       </Select>
     </FormControl>
   )
