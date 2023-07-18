bbdea2493c120965af6d92e8b8b809b2b5a27847
happy <kjong0819@naver.com>
2023-07-18 15:13:56 +0900

    Refactoring codebase

--- src/components/atoms/ProfileCard.tsx
+++ src/components/atoms/ProfileCard.tsx
@@ -1,41 +1,19 @@
 import React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import styled from 'styled-components';
-import { Box, IconButton } from '@mui/material';
+import { Avatar, Box, Grid, IconButton, Typography } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 import { useNavigate } from 'react-router-dom';
 import { UserTagList } from './UserTagList';
 
-interface ProfileCardProps {
-  user: {
-    name: string;
-    age: number;
-    email: string;
-    profilePic: string;
-  };
+interface User {
+  name: string;
+  age: number;
+  email: string;
+  profilePic: string;
 }
 
-const StyledGrid = styled(Grid)`
-  //background: #ffdbdb;
-  color: #000000;
-  padding: 16px;
-  align-items: center;
-  justify-content: center;
-`;
-
-const StyledContentsGrid = styled(Grid)`
-  color: #000000;
-  padding: 16px;
-`;
-
-const OuterGrid = styled(Grid)`
-  background-color: white;
-  border-radius: 10px;
-  //border: 1px gray solid;
-  padding: 5%;
-`;
+interface ProfileCardProps {
+  user: User;
+}
 
 const ProfileCard: React.FC<ProfileCardProps> = ({ user }) => {
   const navigate = useNavigate();
@@ -45,49 +23,57 @@ const ProfileCard: React.FC<ProfileCardProps> = ({ user }) => {
   };
 
   return (
-    <OuterGrid>
-      <StyledGrid container>
-        <Grid item xs={11}></Grid>
+    <Box bgcolor="white" borderRadius={10} padding="5%">
+      <Grid container alignItems="center">
+        <Grid item xs={11}>
+          <Typography variant="h5" gutterBottom>
+            {user.name}
+          </Typography>
+        </Grid>
         <Grid item xs={1}>
-          <IconButton aria-label={'Modify'} onClick={handleEditClick}>
+          <IconButton aria-label="Modify" onClick={handleEditClick}>
             <EditIcon />
           </IconButton>
         </Grid>
-      </StyledGrid>
-      <StyledGrid container>
-        <Avatar
-          sx={{
-            width: '85%', // 작은 화면에서는 100% 너비로 설정
-            maxWidth: '250px', // 큰 화면에서는 최대 200px 크기로 설정
-            height: '85%', // 높이는 자동으로 조정
-            maxHeight: '250px',
-          }}
-          alt="Profile Picture"
-          src={user.profilePic}
-        />
-        <Grid item xs={12}></Grid>
-        <Typography variant="h5" gutterBottom>
-          {user.name}
-        </Typography>
-      </StyledGrid>
+      </Grid>
+      <Grid container>
+        <Typography variant="subtitle1" color="textSecondary" gutterBottom>
+            프로필 사진
+          </Typography>
+          <Avatar
+            sx={{
+              width: '85%',
+              maxWidth: '250px',
+              height: 'auto',
+              maxHeight: '250px',
+            }}
+            alt="Profile Picture"
+            src={user.profilePic}
+          />
+      </Grid>
 
-      <StyledContentsGrid container>
+      <Grid container>
         <Box>
           <Typography variant="subtitle1" gutterBottom>
-            소개
+            나이
           </Typography>
           <Typography variant="body1" color="textSecondary" gutterBottom>
             {user.age}
           </Typography>
           <Typography variant="subtitle1" color="textSecondary" gutterBottom>
-            회사 키워드
+            이메일
           </Typography>
           <Typography variant="body1" color="textSecondary" gutterBottom>
-            <UserTagList />
+            {user.email}
           </Typography>
+          
+          <Typography variant="subtitle1" color="textSecondary" gutterBottom>
+            회사 키워드
+          </Typography>
+          <UserTagList />
         </Box>
-      </StyledContentsGrid>
-    </OuterGrid>
+      </Grid>
+    </Box>
   );
 };
 
